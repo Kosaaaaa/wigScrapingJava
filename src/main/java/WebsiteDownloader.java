a21fcf077e92b1c6d39340faf34b0af20c0ee6e5
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-03 12:44:14 +0100

    add WebsiteDownloader

---
+++ src/main/java/WebsiteDownloader.java
@@ -0,0 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class WebsiteDownloader {
+    private String url;
+    private Document doc;
+
+    public WebsiteDownloader(String url) {
+        try{
+            this.doc = Jsoup.connect(url).get();
+        }catch (IOException e){
+            System.out.println("Website could not be downloaded");
+        }
+    }
+
+    public Document getDoc() {
+        return doc;
+    }
+
+    public void setDoc(Document doc) {
+        this.doc = doc;
+    }
+}
