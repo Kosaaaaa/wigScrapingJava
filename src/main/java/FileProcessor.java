56e238458ea39a645b1595bdca02dbc88efc2538
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-04 10:27:22 +0100

    add FileProcessor class

---
+++ src/main/java/FileProcessor.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class FileProcessor {
+    private final String filename;
+
+    public FileProcessor(String filename) {
+        this.filename = filename;
+        createFile();
+    }
+
+    private void createFile() {
+        try {
+            File file = new File(this.filename);
+            if (file.createNewFile()) {
+                System.out.println(ConsoleColors.ANSI_GREEN + "File created: " + file.getName() + ConsoleColors.ANSI_RESET);
+            } else {
+                System.out.println(ConsoleColors.ANSI_YELLOW + "File already exists." + ConsoleColors.ANSI_RESET);
+            }
+        } catch (IOException e) {
+            System.out.println(ConsoleColors.ANSI_RED + "An error occurred while creating file." + ConsoleColors.ANSI_RESET);
+        }
+    }
+
+    public void writeLines(List<String> lines) {
+        try {
+            FileWriter writer = new FileWriter(this.filename);
+
+            for (String line : lines) {
+                writer.write(line + "\n");
+            }
+
+            writer.close();
+            System.out.println(ConsoleColors.ANSI_GREEN + "Successfully wrote to the file." + ConsoleColors.ANSI_RESET);
+        } catch (IOException e) {
+            System.out.println(ConsoleColors.ANSI_RED + "An error occurred while writing to file." + ConsoleColors.ANSI_RESET);
+        }
+    }
+}
