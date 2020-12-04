af7f90d264bb0ddf679f3368b455e556d855e813
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-04 10:29:18 +0100

    add get and write shares

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String URL = "https://stooq.pl/t/?i=582";
+        WebsiteDownloader website = getWebsite(URL);
+        Scraper scraper = new Scraper(website.getDoc());
+        List<Share> shares = scraper.getShares();
+        List<String> sharesLines = new ArrayList<>();
+        FileProcessor fileProcessor = new FileProcessor("stocks.txt");
+        for (Share share : shares){
+            System.out.println(share.toString());
+            sharesLines.add(share.toString());
+        }
+        fileProcessor.writeLines(sharesLines);
+    }
+
+    public static WebsiteDownloader getWebsite(String URL) {
+        return new WebsiteDownloader(URL);
     }
 }
