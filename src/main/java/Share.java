b781e92847572139025388ba14491d4b386ad795
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-04 10:33:35 +0100

    delete unused getters

--- src/main/java/Share.java
+++ src/main/java/Share.java
@@ -20,48 +20,24 @@ public class Share {
         this.timestamp = timestamp;
     }
 
-    public String getSymbol() {
-        return symbol;
-    }
-
     public void setSymbol(String symbol) {
         this.symbol = symbol;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
     public void setPrice(double price) {
         this.price = price;
-    }
-
-    public double getChangePercent() {
-        return changePercent;
     }
 
     public void setChangePercent(double changePercent) {
         this.changePercent = changePercent;
     }
 
-    public double getVolume() {
-        return volume;
-    }
-
     public void setVolume(double volume) {
         this.volume = volume;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
     }
 
     public void setTimestamp(Date timestamp) {
