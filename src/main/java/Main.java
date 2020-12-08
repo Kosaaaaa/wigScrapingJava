f80630ae781285ce861a2b587a43379a8d195f01
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-08 18:57:41 +0100

    add save options selection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,18 +9,47 @@ public class Main {
         WebsiteDownloader website = getWebsite(URL);
         Scraper scraper = new Scraper(website.getDoc());
         List<Share> shares = scraper.getShares();
-        for (Share share : shares){
+        for (Share share : shares) {
             System.out.println(share.toString());
         }
-        saveInTextFile(shares);
+        System.out.println(ConsoleColors.ANSI_YELLOW + "Where to save shares data?: " + ConsoleColors.ANSI_RESET);
+        System.out.println(ConsoleColors.ANSI_PURPLE + "1. Text file" + ConsoleColors.ANSI_RESET);
+        System.out.println(ConsoleColors.ANSI_PURPLE + "2. Excel file" + ConsoleColors.ANSI_RESET);
+        System.out.println(ConsoleColors.ANSI_PURPLE + "0. Quit" + ConsoleColors.ANSI_RESET);
+        Scanner inp = new Scanner(System.in);
+
+        int selection = 0;
+        try {
+            selection = inp.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println(ConsoleColors.ANSI_RED + "Select correct option." + ConsoleColors.ANSI_RESET);
+        }
+
+        switch (selection) {
+            case 0:
+                System.out.println(ConsoleColors.ANSI_YELLOW + "See you next time!" + ConsoleColors.ANSI_RESET);
+                break;
+            case 1:
+                saveInTextFile(shares);
+                break;
+            case 2:
+                saveInExcelFile(shares);
+                break;
+        }
     }
 
     public static WebsiteDownloader getWebsite(String URL) {
         return new WebsiteDownloader(URL);
     }
-    public static void saveInTextFile(List<Share> shares){
+
+    public static void saveInExcelFile(List<Share> shares) {
+        ExcelFileProcessor excelFileProcessor = new ExcelFileProcessor("stocks.xlsx");
+        excelFileProcessor.writeLines(shares);
+    }
+
+    public static void saveInTextFile(List<Share> shares) {
         List<String> sharesLines = new ArrayList<>();
-        for (Share share : shares){
+        for (Share share : shares) {
             sharesLines.add(share.toString());
         }
         TextFileProcessor textFileProcessor = new TextFileProcessor("stocks.txt");
